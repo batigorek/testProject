20027a9ce43030dc1aec3a16006759bcebc6dbad
batigorek <batigorek@gmail.com>
2021-10-03 21:10:32 +0300

    Categories

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import News
+from .models import News, Category
 
 # Create your views here.
 
 def index(request):
     news = News.objects.all()
-    return render(request, 'news/index.html', {'news': news, 'title': 'List of news'})
+    categories = Category.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+        'categories': categories
+    }
+    return render(request, 'news/index.html', context=context)
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category': category})
